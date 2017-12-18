44c7772e2912c17f177d2d766cd12a6bdebe0744
Moritz Schwörer <mr.mosch@gmail.com>
2017-12-18 14:43:19 +0100

    more tests

--- src/molecules/Card/CardOverlayEditor.test.js
+++ src/molecules/Card/CardOverlayEditor.test.js
@@ -2,20 +2,69 @@ import React from 'react'
 import CardOverlayEditor from './CardOverlayEditor'
 import CardButton from './CardButton'
 
-test('Test component alert when cancel button is clicked', () => {
-
-
-  const log = jest.fn();
+test('Component sends confirm when cancel button is clicked', () => {
+  const log = jest.fn()
+  const requestClose = jest.fn()
+  const onSave = jest.fn()
   global.window.confirm = log
-  const example = mount(      <CardOverlayEditor
-          // eslint-disable-next-line
-          initialText = "test"
-          onSave={jest.fn()}
-          onRequestClose={jest.fn()}
-          confirmText={"cancel forsure"}
-          cancelText={"buttoncancel"}
-          submitText={"sumbit"}
-        />);
-  example.find(CardButton).at(0).simulate("click")
+  const example = mount(
+    <CardOverlayEditor
+      initialText="test"
+      onSave={onSave}
+      onRequestClose={requestClose}
+      confirmText="cancel forsure"
+      cancelText="buttoncancel"
+      submitText="sumbit"
+    />
+  )
+  example
+    .find(CardButton)
+    .at(0)
+    .simulate('click')
   expect(log).toHaveBeenCalledWith('cancel forsure')
+  expect(requestClose).toHaveBeenCalledTimes(0)
+  expect(onSave).toHaveBeenCalledTimes(0)
+})
+
+test('Should close when cancel button is clicked without text', () => {
+  const requestClose = jest.fn()
+  const save = jest.fn()
+  const wrapper = mount(
+    <CardOverlayEditor
+      onSave={save}
+      onRequestClose={requestClose}
+      confirmText="cancel forsure"
+      cancelText="buttoncancel"
+      submitText="sumbit"
+    />
+  )
+
+  wrapper
+    .find(CardButton)
+    .at(0)
+    .simulate('click')
+  expect(requestClose).toHaveBeenCalled()
+})
+
+test('Should save when confirm button is clicked', () => {
+  const requestClose = jest.fn()
+  const save = jest.fn()
+  const wrapper = mount(
+    <CardOverlayEditor
+      onSave={save}
+      initialText="Hello World"
+      onRequestClose={requestClose}
+      confirmText="cancel forsure"
+      cancelText="buttoncancel"
+      submitText="sumbit"
+    />
+  )
+
+  wrapper
+    .find(CardButton)
+    .at(1)
+    .simulate('click')
+
+  expect(requestClose).toHaveBeenCalledTimes(0)
+  expect(save).toHaveBeenCalled()
 })
