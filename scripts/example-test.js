bcc106c39e1d6e5b27b6b8ca66dcf215020dba46
kialysa <kialysa@yahoo.com>
2018-01-08 23:05:47 -0800

    added eval and requires

--- scripts/example-test.js
+++ scripts/example-test.js
@@ -2,28 +2,18 @@ import glob from 'glob'
 import { promisify } from 'util'
 import fs from 'fs'
 import path from 'path'
-import View from '../src/atoms/View'
-import { transform } from 'babel-core';
-import React from 'react'
+import { transform } from 'babel-core'
 var babel = require("babel-core");
+var React = require('react');
+var View = require("../src/atoms/View");
 
 const reactDocs = require('react-docgen')
 
-const writeFile = promisify(fs.writeFile)
 const readfile = promisify(fs.readFile)
 const globber = promisify(glob)
 
-function propToRow(props) {
-  return propName => {
-    const { required, description, defaultValue, type } = props[propName]
-    return `|${propName}${required ? ' **(required)**' : ''}|${type.name}|${
-      description ? description : ''
-    }${defaultValue ? `<br>Default: ${defaultValue.value}` : ''}`
-  }
-}
 const retrieveExamples = (async function() {
   try {
-    console.log('test test test')
     let files = await globber('src/**/*.jsx')
     files = files.filter(path => path.indexOf('.test.jsx') === -1)
     files = await Promise.all(
@@ -50,39 +40,10 @@ const retrieveExamples = (async function() {
         //console.log('more than 1!', wholeExample)
         wholeExample.forEach(example=> {
           const result = babel.transform(example, {babelrc: true, presets: ['react']})
-          console.log(eval('var React= require("react"); var View = require("../src/atoms/View");' + result.code))
+          console.log(eval(result.code))
         })
       }
-      return ({
-      file,
-      docs: `<!-- 
-This is an auto-generated markdown. 
-You can change it in "${file}" and run build:docs to update this file.
--->
-# ${docs.displayName}
-${docs.description}
-## Usage
-| Name        | Type           | Description  |
-| ----------- |:--------------:| ------------:|
-${docs.props ?
-        Object.keys(docs.props)
-          .map(propToRow(docs.props))
-          .join('\n') : '*No properties to pass*'}
-`,
-    })})
-/*
-    const write = await Promise.all(
-      files.map(({ file, docs }) => {
-        writeFile(
-          __dirname +
-            '/../' +
-            file.replace('src/', 'doc/reference/').replace('.jsx', '.md'),
-          docs,
-          { flag: 'w+' }
-        )
-      })
-    )
-*/
+      return (wholeExample)})
   } catch (e) {
     console.log(e)
   }
