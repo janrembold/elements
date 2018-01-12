e575cf63d450359f9fda07eab291ef62f0665062
kialysa <kialysa@yahoo.com>
2018-01-12 16:23:25 +0100

    testing working for 1 example

--- scripts/example.test.js
+++ scripts/example.test.js
@@ -4,35 +4,40 @@ import { transform } from 'babel-core'
 var { View, ThemeProvider, Spinner } = require('./atoms')
 var React = require('react');
 
+/*
 describe('Ladeeda', async () => {
+  const randomShit = retrieveExamples.then(result => {
+    //console.log('restul result result', result)
+    result.map(example => {
+      const { wholeExample, file, displayName } = example
+      if (displayName === 'Spinner') {
+        wholeExample.forEach(stringExample => {
+          console.log('working working', stringExample)
+          console.log('spinner3', Spinner)
+          const transformedString = transform(stringExample, {presets: ['react'], plugins: ['transform-react-jsx']}, function(err, result) {
+            console.log('result is', result)
+          })
+          //console.log('resulttwo is', transformedString)
+          //const Spin = eval('console.log("eval spiner is");' + resultTwo.code)
+          //const component = shallow(Spin)
+          //console.log('prop is', Spin)
+        })
+      }
+    })
+    return('jello')
+  }).catch(err => {
+    console.log(err)
+    return('mistake')
+  })
+  randomShit.then(result => console.log('awaited', result))
   it('should match', async () => {
     //const examples = await retrieveExamples
-    const randomShit = retrieveExamples.then(result => {
-      //console.log('restul result result', result)
-      result.map(example => {
-        const { wholeExample, file, displayName } = example
-        if (displayName === 'Spinner') {
-          wholeExample.forEach(stringExample => {
-            console.log('working working', stringExample)
-            console.log('spinner3', Spinner)
-            const resultTwo = transform(stringExample, {presets: ['react']})
-            console.log('resulttwo is', resultTwo)
-            //const Spin = eval('console.log("eval spiner is");' + resultTwo.code)
-            //const component = shallow(Spin)
-            //console.log('prop is', Spin)
-          })
-        }
-      })
-      return('jello')
-    }).catch(err => {
-      console.log(err)
-      return('mistake')
-    })
-    randomShit.then(result => console.log('awaited', result))
+    
   })
 })
+*/
 
-/*
+
 describe('Ladeeda', async () => {
   it('should match', async () => {
     const examples = await retrieveExamples
@@ -40,18 +45,19 @@ describe('Ladeeda', async () => {
       const { wholeExample, file, displayName } = example
       //console.log('example is', displayName)
       if (displayName === 'Spinner') {
-        wholeExample.forEach(stringExample => {
+        wholeExample.forEach((stringExample, index) => {
           const result = transform(stringExample, {presets: ['react']})
           console.log('result.code', result.code)
           const Spin = eval(result.code)
           const component = shallow(Spin)
           console.log('prop is', component)
+          expect(component).toMatchSnapshot(displayName + index)
         })
       }
     })
   })
 })
-*/
+
 
 
 
