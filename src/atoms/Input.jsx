a0bd1ab525e36dba1509fe5b7d501562c881d5db
Daniel Lehr <daniel.lehr@allthings.me>
2018-03-16 10:43:30 +0100

    Add error prop for server side errors.

--- src/atoms/Input.jsx
+++ src/atoms/Input.jsx
@@ -127,20 +127,31 @@ class Input extends React.Component {
     maxLength: PropTypes.number,
     /** Called with the input field a reference */
     onInputRef: PropTypes.func,
+    /** An error can be passed to the input field, for example for server side errors */
+    error: PropTypes.node,
+    /** Is called when the error should not be displayed anymore */
+    onRequestErrorClose: PropTypes.func,
   }
 
   state = {
     value: '',
     visible: true,
-    message: null,
+    message: this.props.error,
     length: (this.props.value && this.props.value.length) || 0,
   }
 
   static defaultProps = {
     required: false,
     lines: 1,
-    onInputRef: _ => _,
+    onInputRef: () => {},
+    onRequestErrorClose: () => {},
     type: 'text',
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.error !== nextProps.error) {
+      this.setState({ message: nextProps.error })
+    }
   }
 
   handleInvalid = e => {
@@ -180,10 +191,22 @@ class Input extends React.Component {
     this.props.onChange && this.props.onChange(e)
   }
 
-  handleMessageClick = () => this.setState({ message: null })
+  handleMessageClick = () => {
+    this.setState({ message: null })
+    this.props.onRequestErrorClose()
+  }
 
   render() {
-    const { required, onInputRef, lines, label, pattern, ...props } = this.props
+    const {
+      required,
+      onInputRef,
+      lines,
+      label,
+      pattern,
+      error,
+      onRequestErrorClose,
+      ...props
+    } = this.props
     const currentValue = this.props.value || this.state.value
     const labelVisible = currentValue.length > 0
     const showLabel = label && currentValue.length > 0
