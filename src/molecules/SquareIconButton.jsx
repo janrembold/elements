6ecd66901113c7ebe7be317788954c5e5a4cd80b
Moritz Schwörer <moritz.schwoerer@allthings.me>
2018-06-22 11:00:18 +0200

    Update SquareIconButton.jsx

--- src/molecules/SquareIconButton.jsx
+++ src/molecules/SquareIconButton.jsx
@@ -47,7 +47,7 @@ class SquareIconButton extends React.Component {
   }
 
   render() {
-    const { icon, color, iconColor, iconSize, onClick, ...restProps } = this.props
+    const { icon, color, iconColor, iconSize, onClick, ...props } = this.props
     return (
       <View
         {...box(colorCode(color), onClick)}
@@ -55,7 +55,7 @@ class SquareIconButton extends React.Component {
         alignH="center"
         alignV="center"
         onClick={onClick}
-        {...restProps}
+        {...props}
       >
         <Icon color={iconColor} size={iconSize} name={icon} />
       </View>
