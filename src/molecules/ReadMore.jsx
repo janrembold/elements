6392b9e1c16938a35972267a7d3ccf999d30be23
Michael Henning <mh@fivenp.com>
2018-06-28 12:46:45 +0200

    readMore baby...
    Signed-off-by: Michael Henning <mh@fivenp.com>

--- src/molecules/ReadMore.jsx
+++ src/molecules/ReadMore.jsx
@@ -2,12 +2,31 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { css } from 'glamor'
 import { View, Text } from '../'
-import { ColorPalette, alpha } from '@allthings/colors'
 
 /**
- *   A ReadMore is a simple container, to show / hide content
+ *   A ReadMore is a simple container, to show / hide content. It will automatically decide whether to show the `read more` link or not.
  * ```example
-
+<Card>
+  <ListItem>
+    <ReadMore>
+      <Text>Testing a short text...</Text>
+    </ReadMore>
+  </ListItem>
+  <ListItem>
+    <ReadMore>
+      <Text>
+        Testing a longer text with a defaultHeight 80vw! Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit. Maecenas
+        dignissim sem in elit mollis consequat. Suspendisse potenti.
+        Maecenas a velit vel dolor mollis viverra. Praesent ex diam,
+        ultricies ac ultricies ut, efficitur sit amet leo. Vivamus ex
+        ante, dapibus a elementum vel, ultrices in erat. Vestibulum
+        eget ante turpis. Donec dapibus, purus vel euismod egestas,
+        arcu ipsum.
+      </Text>
+    </ReadMore>
+  </ListItem>
+</Card>
  *  ```
  **/
 
@@ -16,37 +35,34 @@ class ReadMore extends React.Component {
     children: PropTypes.node,
     initiallyCollapsed: PropTypes.bool,
     readMoreLabel: PropTypes.string,
-    readLessLabel: PropTypes.string,
+    defaultHeight: PropTypes.oneOfType(PropTypes.string, PropTypes.number),
     toggleCallback: PropTypes.func,
   }
 
   static defaultProps = {
     initiallyCollapsed: true,
     readMoreLabel: 'Read more...',
-    showLessLabel: 'Show Less...',
+    defaultHeight: '20vh',
     toggleCallback: () => {},
   }
 
   state = { collapsed: this.props.initiallyCollapsed }
 
   componentDidMount() {
-    if (this.childRef.current) {
-      if (!this.props.initiallyCollapsed) {
-        this.childRef.current.style.height = `${
-          this.childRef.current.scrollHeight
-        }px`
-      } else {
-        this.childRef.current.style.height = `20vh`
-      }
-    }
+    this.toggleCollapseLink()
+    window.addEventListener('resize', this.toggleCollapseLink.bind(this))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.toggleCollapseLink.bind(this))
   }
 
   childRef = React.createRef()
 
   toggleCollapse = () => {
     const { current } = this.childRef
-    if (current.style.height !== '20vh') {
-      current.style.height = '20vh'
+    if (!this.state.collapsed) {
+      current.style.height = this.props.defaultHeight
       this.setState({ collapsed: true })
       // signal new state for the parent
       this.props.toggleCallback(true)
@@ -58,10 +74,46 @@ class ReadMore extends React.Component {
     }
   }
 
-  onKeyPress = e => e.key === 'Enter' && this.toggleCollapse()
+  toggleCollapseLink = () => {
+    const { current } = this.childRef
+    console.log(current.style.height, current.scrollHeight)
+    console.log(window.innerHeight)
+
+    let defaultWrapperHeight = 0
+    let elHeight = current.scrollHeight
+
+    // Let's check for viewport dimensions in here and convert them to px...
+    const regex = /(vw|vh)$/
+    if (regex.test(this.props.defaultHeight)) {
+      if (this.props.defaultHeight.match(/(vh)/)) {
+        defaultWrapperHeight =
+          (window.innerHeight / 100) *
+          parseInt(this.props.defaultHeight.replace(regex, ''))
+      }
+      if (this.props.defaultHeight.match(/(vw)/)) {
+        defaultWrapperHeight =
+          (window.innerWidth / 100) *
+          parseInt(this.props.defaultHeight.replace(regex, ''))
+      }
+    } else {
+      defaultWrapperHeight = this.props.defaultHeight
+    }
+
+    // Let's check whether we should show the Read More link or not
+    if (elHeight < defaultWrapperHeight) {
+      current.style.height = `${elHeight}px`
+      this.props.toggleCallback(false)
+      this.setState({ collapsed: false })
+    } else {
+      current.style.height = `${defaultWrapperHeight}px`
+      this.props.toggleCallback(true)
+      this.setState({ collapsed: true })
+    }
+    // console.log('elHeight', elHeight, 'wrapperHeight', defaultWrapperHeight)
+  }
 
   render() {
-    const { children, readMoreLabel, showLessLabel } = this.props
+    const { children, readMoreLabel } = this.props
     return (
       <View
         direction="column"
@@ -81,21 +133,27 @@ class ReadMore extends React.Component {
         >
           {children}
         </View>
-        <View
-          onClick={this.toggleCollapse}
-          {...css({
-            paddingTop: this.state.collapsed ? 35 : 10,
-            marginTop: this.state.collapsed ? -30 : 0,
-            background: 'transparent',
-            background: `-moz-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 57%, rgba(255,255,255,1) 100%)`,
-            background: `-webkit-linear-gradient(top, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 57%,rgba(255,255,255,1) 100%)`,
-            background: `linear-gradient(to bottom, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 57%,rgba(255,255,255,1) 100%)`,
-          })}
-        >
-          <Text size="xs">
-            {this.state.collapsed ? readMoreLabel : showLessLabel}
-          </Text>
-        </View>
+        {this.state.collapsed && (
+          <View
+            onClick={this.toggleCollapse}
+            {...css({
+              cursor: 'pointer',
+              paddingTop: this.state.collapsed ? 35 : 10,
+              marginTop: this.state.collapsed ? -30 : 0,
+              background: 'transparent',
+              // eslint-disable-next-line no-dupe-keys
+              background: `-moz-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 57%, rgba(255,255,255,1) 100%)`,
+              // eslint-disable-next-line no-dupe-keys
+              background: `-webkit-linear-gradient(top, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 57%,rgba(255,255,255,1) 100%)`,
+              // eslint-disable-next-line no-dupe-keys
+              background: `linear-gradient(to bottom, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 57%,rgba(255,255,255,1) 100%)`,
+            })}
+          >
+            <Text size="s" {...css({ opacity: 0.7 })}>
+              {readMoreLabel}
+            </Text>
+          </View>
+        )}
       </View>
     )
   }
--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import {
   SimpleLayout,
   View,
   Card,
+  ListItem,
   Icon,
 } from '../src/'
 import HorizontalView from './HorizontalView'
@@ -83,41 +84,91 @@ storiesOf('ReadMore', module)
       <ThemeProvider>
         <SimpleLayout>
           <Card>
-            <ReadMore>
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas dignissim sem in elit mollis consequat. Suspendisse
-                potenti. Maecenas a velit vel dolor mollis viverra. Praesent ex
-                diam, ultricies ac ultricies ut, efficitur sit amet leo. Vivamus
-                ex ante, dapibus a elementum vel, ultrices in erat. Vestibulum
-                eget ante turpis. Donec dapibus, purus vel euismod egestas, arcu
-                ipsum gravida nunc, sed porta justo ipsum in eros. Maecenas
-                tristique sollicitudin interdum. Duis arcu justo, pretium quis
-                nunc nec, lacinia vehicula sapien. Aenean bibendum volutpat
-                magna, sit amet ultricies dolor viverra eu. Proin massa ex,
-                interdum id porta sed, placerat ac dui. Aliquam erat volutpat.
-                Quisque at facilisis erat. Mauris sodales odio felis, sed
-                malesuada neque faucibus et. Vivamus convallis tellus in nunc
-                feugiat egestas. Cras mattis tempus felis sed tristique. Nullam
-                purus nisi, tristique eu tempus quis, tristique eget enim. Nulla
-                tincidunt nulla sit amet nunc lacinia, maximus posuere quam
-                pharetra. Ut sit amet posuere metus. Vestibulum vehicula nulla
-                non nibh dictum bibendum. Phasellus arcu nibh, cursus sit amet
-                interdum sit amet, maximus vel massa. Aliquam porta urna non
-                orci porttitor varius. Donec et scelerisque odio. Sed quis
-                condimentum nisl, id facilisis urna. Curabitur id erat a mauris
-                placerat sodales. Cras in elementum lacus. Morbi eget varius ex.
-                Nulla commodo sem odio, vel tempus nunc imperdiet quis. Maecenas
-                eu pharetra quam, id fermentum turpis. Curabitur consectetur
-                commodo vestibulum. Nunc imperdiet dolor sit amet lacinia
-                varius. Praesent consequat nisl elit, et euismod nisi posuere
-                vel. Maecenas ullamcorper, ante et consequat convallis, dolor
-                neque elementum ante, consectetur egestas sapien eros elementum
-                arcu. Nulla metus erat, congue eget nibh sed, efficitur
-                venenatis velit. Ut nec sagittis sapien. Fusce eu ligula sed
-                metus tempus vulputate ac vel lacus. Aenean ut libero sem.
-              </Text>
-            </ReadMore>
+            <ListItem>
+              <ReadMore>
+                <Text>Testing a short text...</Text>
+              </ReadMore>
+            </ListItem>
+            <ListItem>
+              <ReadMore defaultHeight={10}>
+                <Text>Testing a short text with a defaultHeight 10px!...</Text>
+              </ReadMore>
+            </ListItem>
+            <ListItem>
+              <ReadMore defaultHeight="80vw">
+                <Text>
+                  Testing a longer text with a defaultHeight 80vw! Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit. Maecenas
+                  dignissim sem in elit mollis consequat. Suspendisse potenti.
+                  Maecenas a velit vel dolor mollis viverra. Praesent ex diam,
+                  ultricies ac ultricies ut, efficitur sit amet leo. Vivamus ex
+                  ante, dapibus a elementum vel, ultrices in erat. Vestibulum
+                  eget ante turpis. Donec dapibus, purus vel euismod egestas,
+                  arcu ipsum.
+                </Text>
+              </ReadMore>
+            </ListItem>
+            <ListItem>
+              <ReadMore>
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Maecenas dignissim sem in elit mollis consequat. Suspendisse
+                  potenti. Maecenas a velit vel dolor mollis viverra. Praesent
+                  ex diam, ultricies ac ultricies ut, efficitur sit amet leo.
+                  Vivamus ex ante, dapibus a elementum vel, ultrices in erat.
+                  Vestibulum eget ante turpis. Donec dapibus, purus vel euismod
+                  egestas, arcu ipsum.
+                </Text>
+              </ReadMore>
+            </ListItem>
+            <ListItem>
+              <ReadMore>
+                <Text strong>Works also with content blocks...</Text>
+                <img
+                  src="https://picsum.photos/200/300"
+                  style={{ width: '100%' }}
+                />
+              </ReadMore>
+            </ListItem>
+            <ListItem>
+              <ReadMore>
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Maecenas dignissim sem in elit mollis consequat. Suspendisse
+                  potenti. Maecenas a velit vel dolor mollis viverra. Praesent
+                  ex diam, ultricies ac ultricies ut, efficitur sit amet leo.
+                  Vivamus ex ante, dapibus a elementum vel, ultrices in erat.
+                  Vestibulum eget ante turpis. Donec dapibus, purus vel euismod
+                  egestas, arcu ipsum gravida nunc, sed porta justo ipsum in
+                  eros. Maecenas tristique sollicitudin interdum. Duis arcu
+                  justo, pretium quis nunc nec, lacinia vehicula sapien. Aenean
+                  bibendum volutpat magna, sit amet ultricies dolor viverra eu.
+                  Proin massa ex, interdum id porta sed, placerat ac dui.
+                  Aliquam erat volutpat. Quisque at facilisis erat. Mauris
+                  sodales odio felis, sed malesuada neque faucibus et. Vivamus
+                  convallis tellus in nunc feugiat egestas. Cras mattis tempus
+                  felis sed tristique. Nullam purus nisi, tristique eu tempus
+                  quis, tristique eget enim. Nulla tincidunt nulla sit amet nunc
+                  lacinia, maximus posuere quam pharetra. Ut sit amet posuere
+                  metus. Vestibulum vehicula nulla non nibh dictum bibendum.
+                  Phasellus arcu nibh, cursus sit amet interdum sit amet,
+                  maximus vel massa. Aliquam porta urna non orci porttitor
+                  varius. Donec et scelerisque odio. Sed quis condimentum nisl,
+                  id facilisis urna. Curabitur id erat a mauris placerat
+                  sodales. Cras in elementum lacus. Morbi eget varius ex. Nulla
+                  commodo sem odio, vel tempus nunc imperdiet quis. Maecenas eu
+                  pharetra quam, id fermentum turpis. Curabitur consectetur
+                  commodo vestibulum. Nunc imperdiet dolor sit amet lacinia
+                  varius. Praesent consequat nisl elit, et euismod nisi posuere
+                  vel. Maecenas ullamcorper, ante et consequat convallis, dolor
+                  neque elementum ante, consectetur egestas sapien eros
+                  elementum arcu. Nulla metus erat, congue eget nibh sed,
+                  efficitur venenatis velit. Ut nec sagittis sapien. Fusce eu
+                  ligula sed metus tempus vulputate ac vel lacus. Aenean ut
+                  libero sem.
+                </Text>
+              </ReadMore>
+            </ListItem>
           </Card>
         </SimpleLayout>
       </ThemeProvider>
