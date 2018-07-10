670ff384463cfc7713ae0a9454004c92b357c34d
Alysa Kikuchi <kialysa@yahoo.com>
2018-07-10 16:02:36 +0200

    revert storybook back for horizontalView

--- stories/HorizontalView.jsx
+++ stories/HorizontalView.jsx
@@ -43,13 +43,15 @@ export default class HorizontalViewStory extends React.Component {
             </TitleBar>
             <View direction="row" flex="flex">
               <HorizontalView>
-                <Text id="first" key="first">
-                  Text 1
-                </Text>
-                <Text id="second" key="second">
-                  Text 2
-                </Text>
-                {undefined}
+                {Array(this.state.level)
+                  .fill({})
+                  .map((_, i) => (
+                    <CardList key={i} direction="column">
+                      <ListItem>
+                        <Text>{`Test ${i}`}</Text>
+                      </ListItem>
+                    </CardList>
+                  ))}
               </HorizontalView>
             </View>
             <View direction="row" alignH="space-around" style={{ margin: 15 }}>
