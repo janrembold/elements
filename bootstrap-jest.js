f64558fbed1e08faa2c698aece74bbb222ef076e
Davy Duperron <yamafaktory@gmail.com>
2018-08-14 11:10:11 +0200

    Add snapshot-dif & fromDOMNode in the Jest config.

--- bootstrap-jest.js
+++ bootstrap-jest.js
@@ -1,8 +1,10 @@
 import { configure, shallow, render, mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import { createSerializer } from 'enzyme-to-json'
-import renderer from 'react-test-renderer'
+import { toMatchDiffSnapshot, getSnapshotDiffSerializer } from 'snapshot-diff'
+import Adapter from 'enzyme-adapter-react-16'
 import fetch from 'jest-fetch-mock'
+import renderer from 'react-test-renderer'
+import serializer, { fromDOMNode } from 'jest-glamor-react'
 
 // Adapter for React 16.
 configure({ adapter: new Adapter() })
@@ -10,9 +12,15 @@ configure({ adapter: new Adapter() })
 // Add serializer for enzyme-to-json.
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
 
+// Add serializer for jest-glamor-react and snapshot-diff.
+expect.addSnapshotSerializer(getSnapshotDiffSerializer())
+expect.addSnapshotSerializer(serializer)
+expect.extend({toMatchDiffSnapshot})
+
 // Avoid annoying imports in all the tests.
 global.fetch = fetch
 global.mount = mount
 global.render = render
 global.renderer = renderer
 global.shallow = shallow
+global.fromDOMNode = fromDOMNode
