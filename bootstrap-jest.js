1d50fca1b66469babfbc5147395acde9b35d0171
Daniel Lehr <daniel.lehr@allthings.me>
2018-11-07 11:54:28 +0100

    Make adjustments for breaking changes

--- bootstrap-jest.js
+++ bootstrap-jest.js
@@ -3,9 +3,12 @@ import Adapter from 'enzyme-adapter-react-16'
 import { createSerializer } from 'enzyme-to-json'
 import renderer from 'react-test-renderer'
 import fetch from 'jest-fetch-mock'
+import genProps from 'react-generate-props'
 
 // Adapter for React 16.
 configure({ adapter: new Adapter() })
+
+genProps.init()
 
 // Add serializer for enzyme-to-json.
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
