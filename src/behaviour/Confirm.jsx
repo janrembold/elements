89195b8b787177d7b6941113e5f3935e9049fa61
Alysa Kikuchi <kialysa@yahoo.com>
2018-11-21 11:54:08 +0100

    move confirm to behaviour

---
+++ src/behaviour/Confirm.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ConfirmDialog from '../molecules/ConfirmDialog'
+import ThemeProvider from './ThemeProvider'
+
+const confirm = message =>
+  new Promise(resolve => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const resolveAndClean = response => {
+      ReactDOM.unmountComponentAtNode(div)
+      document.body.removeChild(div)
+      resolve(response)
+    }
+
+    ReactDOM.render(
+      <ThemeProvider>
+        <ConfirmDialog
+          onCancel={() => resolveAndClean(false)}
+          onSuccess={() => resolveAndClean(true)}
+        >
+          {message}
+        </ConfirmDialog>
+      </ThemeProvider>,
+      div
+    )
+  })
+
+export default confirm
